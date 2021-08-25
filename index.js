5dcb462096cc29a0553d2a308dd9cbec01f231e7
Tomas Carignano <tomascarignano98@gmail.com>
2021-08-25 15:21:58 -0300

    Environment variables.

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ app.get("/api/courses", (request, response) => {
   response.send([1, 2, 3]);
 });
 
-app.listen(3000, () => {
-  console.log("listening on port 3000");
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`listening on port ${port}...`);
 });
