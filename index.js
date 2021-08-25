14408edffd82185c5aabf8f968dde4988144283b
Tomas Carignano <tomascarignano98@gmail.com>
2021-08-25 15:58:24 -0300

    Handling HTTP GET Requests.

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
 const express = require("express");
 const app = express();
 
+const courses = [
+  { id: 1, name: "course1" },
+  { id: 2, name: "course2" },
+  { id: 3, name: "course3" }
+];
+
 app.get("/", (request, response) => {
   response.send("Hello World");
 });
 
 app.get("/api/courses", (request, response) => {
-  response.send([1, 2, 3]);
+  response.send(courses);
 });
 
 // We use required parameters for essential or required values,
 // whereas query string parameters for anything that is optional.
-app.get("/api/courses/:year/:month", (request, response) => {
-  response.send(request.params);
-  // response.send(request.query);
+app.get("/api/courses/:id", (request, response) => {
+  const course = courses.find(
+    (course) => course.id === parseInt(request.params.id)
+  );
+
+  if (!course) response.status(404).send("Course not found.");
+  response.send(course);
 });
 
 const port = process.env.PORT || 3000;
