da6c82d10d86dbc2b886b0b04ba1a0c92edb8304
Tomas Carignano <tomascarignano98@gmail.com>
2021-08-25 15:40:32 -0300

    Route parameters.

--- index.js
+++ index.js
@@ -9,6 +9,13 @@ app.get("/api/courses", (request, response) => {
   response.send([1, 2, 3]);
 });
 
+// We use required parameters for essential or required values,
+// whereas query string parameters for anything that is optional.
+app.get("/api/courses/:year/:month", (request, response) => {
+  response.send(request.params);
+  // response.send(request.query);
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`listening on port ${port}...`);
